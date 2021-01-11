4131aff83c1bf7a0e641e32abd3fed1066a5995e
HoaiBao <rorolo2011@gmail.com>
2021-01-11 17:58:30 +0700

    update forgot password

--- src/app/containers/ForgotPassword/index.jsx
+++ src/app/containers/ForgotPassword/index.jsx
@@ -3,23 +3,32 @@ import { memo } from 'react';
 import { StyledLayout, StyledTitle, StyledCover } from './styles';
 
 export const ForgotPassword = memo(() => {
-  return (
-    <StyledLayout>
-      <StyledCover>
-        <StyledTitle level={2}>Forgot password</StyledTitle>
-        <Form requiredMark={false} size="large" className="form">
-          <Form.Item name="email" required label="Email">
-            <Input placeholder="Input your email here" />
-          </Form.Item>
-          <Form.Item>
-            <Button style={{ width: '100%' }} type="primary">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
-      </StyledCover>
-    </StyledLayout>
-  );
+  const swithStep = step => {
+    switch (step) {
+      case 0:
+        return (
+          <StyledLayout>
+            <StyledCover>
+              <StyledTitle level={2}>Forgot password</StyledTitle>
+              <Form requiredMark={false} size="large" className="form">
+                <Form.Item name="email" required label="Email">
+                  <Input placeholder="Input your email here" />
+                </Form.Item>
+                <Form.Item>
+                  <Button style={{ width: '100%' }} type="primary">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </StyledCover>
+          </StyledLayout>
+        );
+
+      default:
+        break;
+    }
+  };
+  return <>{swithStep(0)}</>;
 });
 
 export default ForgotPassword;
